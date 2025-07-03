a313646243dcf63c269e981ef37188e0732a619f
yksirotta <yksirotta@gmail.com>
2025-07-03 04:57:01 +0300

    Update entrypoint.sh

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -19,6 +19,6 @@ deploy() {
 trap "kill 0" SIGINT
 
 caddy run --config /etc/caddy/Caddyfile --adapter caddyfile \
-& spacetimedb start --listen-addr 0.0.0.0:3000 /stdb \
+& spacetimedb start --listen-addr 0.0.0.0:3369 /stdb \
 & deploy \
 && wait
